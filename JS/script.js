7dded88cf855542b5798244de107f04b34db5ada
Marcus Vinícius Weil <vinicciusplay10@gmail.com>
2022-09-26 22:17:09 -0300

    Button Events e Sprite error handler

--- JS/script.js
+++ JS/script.js
@@ -4,11 +4,17 @@ const pokemonName = document.querySelector('.pokemon-name');
 const pokemonId = document.querySelector('.pokemon-number');
 const pokemonSprite = document.querySelector('#pokemon');
 const space = document.querySelector('.space');
+const prevBtn = document.querySelector('#prev-btn');
+const NextBtn = document.querySelector('#next-btn');
 const pokeField = document.querySelector('#search');
 const pokeInput = document.querySelector(".poke-input");
 const silhouetteImg = document.querySelector(".silhouetteImg");
 const loading = document.querySelector("#loading");
+
 let number = 1;
+let CurrentPokemon = 1;
+let click = false;
+
 
 
 // ** Funções
@@ -27,7 +33,6 @@ const initValues = () => {
 //Gera um número aleatório que será usado pra buscar um Sprite
 const randomNumber = () => {
     number = Math.floor(Math.random() * 100) + 1
-    console.log(number);
 
     return number;
 };
@@ -97,10 +102,12 @@ const getPokemonInfo = async (pokemon) => {
 
     const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
 
-    //Curto delay pra exibir o Loading
-    const waitFor = delay => new Promise(resolve => setTimeout(resolve, delay));
+    if (!click) {
+        //Curto delay pra exibir o Loading
+        const waitFor = delay => new Promise(resolve => setTimeout(resolve, delay));
 
-    await waitFor(400);
+        await waitFor(400);
+    }
 
     if (res.status == 200) {
         const data = await res.json();
@@ -121,12 +128,15 @@ const showPokemonInfo = async (pokemon) => {
 
     initValues();
 
-
     if (info) {
+        let GifUrl = info["sprites"]["versions"]['generation-v']["black-white"]["animated"]["front_default"];
+        let ImgUrl = info["sprites"]["versions"]['generation-v']["black-white"]["front_default"];
+        let vanillaImg = info["sprites"]["front_default"];
         pokemonName.innerText = info.name;
         pokemonId.innerText = info.id;
         pokemonSprite.classList.remove("hide");
-        pokemonSprite.src = info["sprites"]["versions"]['generation-v']["black-white"]["animated"]["front_default"];
+        CurrentPokemon = info.id
+        pokemonSprite.src = (GifUrl != null) ? GifUrl : ImgUrl != null ? ImgUrl : vanillaImg
     } else {
         silhouette();
         pokemonName.classList.add('notFound');
@@ -144,7 +154,24 @@ pokeField.addEventListener('submit', (e) => {
 
     e.preventDefault()
 
+    click = false;
     const pokemon = pokeInput.value.toLowerCase();
     showPokemonInfo(pokemon);
 
 })
+prevBtn.addEventListener('click', () => {
+    if (CurrentPokemon > 1) {
+        CurrentPokemon--
+        click = true;
+        showPokemonInfo(CurrentPokemon)
+    }
+})
+NextBtn.addEventListener('click', () => {
+    if (CurrentPokemon < 905) {
+        CurrentPokemon++
+        click = true;
+        showPokemonInfo(CurrentPokemon)
+    }
+})
+
+showPokemonInfo(CurrentPokemon)
